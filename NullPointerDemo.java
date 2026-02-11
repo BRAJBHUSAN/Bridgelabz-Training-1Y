6eb2355143a9e5eabc1db7a430064bcba1b5abf3
BRAJBHUSAN <rams013140@gmail.com>
2026-02-12 00:38:28 +0530

    ASSIGNMENT5

---
+++ NullPointerDemo.java
@@ -0,0 +1,33 @@
+
+
+package com.gla.strings;
+
+
+public class NullPointerDemo {
+
+    public static void generateException() {
+        String text = null;
+        System.out.println(text.length());
+    }
+
+    public static void handleException() {
+        String text = null;
+        try {
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException handled");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        try {
+            generateException();
+        } catch (NullPointerException e) {
+            System.out.println("Exception generated");
+        }
+
+        handleException();
+    }
+}
---
+++ TimeZones.java
@@ -0,0 +1,20 @@
+
+
+package com.gla.strings;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class TimeZones {
+
+    public static void main(String[] args) {
+
+        ZonedDateTime gmt = ZonedDateTime.now(ZoneId.of("GMT"));
+        ZonedDateTime ist = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime pst = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+        System.out.println("GMT Time : " + gmt);
+        System.out.println("IST Time : " + ist);
+        System.out.println("PST Time : " + pst);
+    }
+}
